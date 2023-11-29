e9e13a89f1063f04a271a7751bf9fae60f8d3d89
HixRaid <qqwweerrttsava@gmail.com>
2023-11-29 21:56:00 +0300

    Added max email length

--- internal/utils/email.go
+++ internal/utils/email.go
@@ -1,11 +1,22 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+	"unicode/utf8"
+)
 
-const emailRegexpPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"
+const (
+	maxEmailLength     = 255
+	emailRegexpPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"
+)
 
 func IsValidEmail(email string) bool {
 	ok, err := regexp.MatchString(emailRegexpPattern, email)
+
+	length := utf8.RuneCountInString(email)
+	if length > maxEmailLength {
+		return false
+	}
 
 	return ok && err == nil
 }
